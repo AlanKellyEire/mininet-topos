b47b5bccf2bfb573d8668ab775522ffdbaf69cd4
gunjan5 <patelgunjan5@gmail.com>
2015-08-11 11:44:20 -0700

    configurable bandwidth, delay, loss, quque_size for the links
    Signed-off-by: gunjan5 <patelgunjan5@gmail.com>

--- spine_leaf.py
+++ spine_leaf.py
@@ -18,6 +18,7 @@ from mininet.net import Mininet
 from mininet.util import irange,dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.cli import CLI
+from mininet.link import TCLink
 from mininet.node import OVSSwitch, Controller, RemoteController
 
 #global variable here
@@ -25,6 +26,14 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 spineList = [ ]
 leafList = [ ]
 switchList = [ ]
+link1 = dict(bw=100, delay='0ms', loss=0, max_queue_size=10000, use_htb=True)
+link2 = dict(bw=15, delay='2ms', loss=0, max_queue_size=1000, use_htb=True)
+link3 = dict(bw=10, delay='1ms', loss=0, max_queue_size=1000, use_htb=True)
+link4 = dict(bw=5, delay='10ms', loss=0, max_queue_size=500, use_htb=True)
+link5 = dict(bw=1, delay='15ms', loss=0, max_queue_size=100, use_htb=True)
+
+link_spine_leaf = link1
+link_host_leaf = link1
 ######################################
 ###### Define topologies here ########
 ######################################
@@ -58,12 +67,12 @@ class dcSpineLeafTopo(Topo):
            #hosts1 = [ net.addHost( 'h%d' % n ) for n in 3, 4 ]
 
            "connection of the hosts to the left tor switch "
-           self.addLink(host1, leafSwitch)
+           self.addLink(host1, leafSwitch, **link_host_leaf)
            #self.addLink(host12, leafSwitch)
 
        for i in irange(0, k-1):
            for j in irange(0, l-1): #this is to go through the leaf switches
-                 self.addLink(spineList[i], leafList[j])
+                 self.addLink(spineList[i], leafList[j], **link_spine_leaf)
 
 def simpleTest():
    # arugment to run in NOOB, NORMAL, TEST modes
@@ -82,7 +91,7 @@ def simpleTest():
    #section for handling the differnt argumetns.... simpleTest(arg1, arg2, ...) will take in arguments from user
    topo = dcSpineLeafTopo(k=int(sys.argv[1]), l=int(sys.argv[2]))
    switchList = spineList + leafList
-   net = Mininet(  topo=topo, switch=MultiSwitch, build=False )
+   net = Mininet(  topo=topo, switch=MultiSwitch, build=False, link=TCLink )
 
    print "connecting all SWITCHES to controller with cmap"
    cString = "{"
